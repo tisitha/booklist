838975fcce06cb82fc76e42e792d9032617545b8
Tisitha <tisitha007@gmail.com>
2024-11-15 21:38:45 +0530

    update2

--- src/Register.jsx
+++ src/Register.jsx
@@ -4,7 +4,7 @@ import "./assets/loginandreigster.css";
 
 function Register() {
 
-  const baseURL = import.meta.env.VITE_REACT_APP_API_BASE_URL;
+  const baseURL = "http://localhost:8080/";
 
   const navigate = useNavigate();
   const [massage, setMessage] = useState('');
